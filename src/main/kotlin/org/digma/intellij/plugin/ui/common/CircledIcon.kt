5c870968e4c4535d127648f697f877daab783d9a
Asaf Chen <achen@digma.ai>
2023-04-04 12:52:30 +0300

    better CircledIcon

--- src/main/kotlin/org/digma/intellij/plugin/ui/common/CircledIcon.kt
+++ src/main/kotlin/org/digma/intellij/plugin/ui/common/CircledIcon.kt
@@ -3,50 +3,45 @@ package org.digma.intellij.plugin.ui.common
 import com.intellij.util.ui.JBUI
 import java.awt.*
 import javax.swing.Icon
-import javax.swing.JComponent
 import javax.swing.JLabel
 import javax.swing.JPanel
 import javax.swing.SwingConstants
 import kotlin.math.min
 
-class CircledIcon(icon: Icon) : JPanel() {
+class CircledIcon(icon: Icon, margin: Int = 15) : JPanel() {
+
+    private var iconPanel: JPanel = object : JPanel()
+    {
+        init {
+            layout = BorderLayout()
+            isOpaque = false
+            alignmentX = Component.CENTER_ALIGNMENT
+            alignmentY = Component.CENTER_ALIGNMENT
+        }
+
+        override fun paintComponent(g: Graphics) {
+            super.paintComponent(g)
+            val graphics = g as Graphics2D
+            graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
+
+            graphics.color = parent.background
+            val border = super.getBorder()?.getBorderInsets(this)?: JBUI.emptyInsets()
+            val radius = min(width-border.left-border.right, height-border.top-border.bottom)
+            graphics.fillOval(border.left, border.top, radius, radius)
+        }
+    }
 
     init {
         val iconLabel = JLabel(icon)
         iconLabel.isOpaque = false
         iconLabel.horizontalAlignment = SwingConstants.CENTER
         iconLabel.verticalAlignment = SwingConstants.CENTER
+        iconLabel.border = JBUI.Borders.empty(margin)
 
-        val iconPanel = object : JPanel(){
-            var parent: JComponent? = null
-
-            override fun getPreferredSize(): Dimension {
-                return Dimension(80,80)
-            }
-
-            override fun paintComponent(g: Graphics) {
-                super.paintComponent(g)
-                val graphics = g as Graphics2D
-                graphics.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON)
-
-                graphics.color = parent?.getBackground() ?: super.getBackground()
-                val border = super.getBorder()?.getBorderInsets(this)?: JBUI.emptyInsets()
-                val radius = min(width-border.left-border.right, height-border.top-border.bottom)
-                graphics.fillOval(border.left, border.top, radius, radius)
-            }
-        }
-        iconPanel.parent = this
-        iconPanel.layout = BorderLayout()
-        iconPanel.isOpaque = false
-        iconPanel.alignmentX = Component.CENTER_ALIGNMENT
-        iconPanel.alignmentY = Component.CENTER_ALIGNMENT
         iconPanel.add(iconLabel)
 
         this.add(iconPanel)
         this.isOpaque = false
     }
 
-    override fun setBackground(bg: Color?) {
-        super.setBackground(bg)
-    }
 }
